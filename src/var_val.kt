77e5228bb80e7ae926bb2fcc0edc7e1a7175d382
selcuk <benimpostahesabim@gmail.com>
2017-09-09 22:48:03 +0300

    var_val

---
+++ src/var_val.kt
@@ -0,0 +1,19 @@
+fun main(Args:Array<String>){
+    var firstVAR = "Selçuk"
+    println(firstVAR)
+
+    firstVAR = "Piraye"
+    println(firstVAR)
+
+    firstVAR = 12.toString()
+    println(firstVAR)
+
+    val firstVAL = "is kotlin awesome?"
+    println(firstVAL)
+
+    /* firstVAL = "may be" val cannot be reassigned */
+
+    // if you want to use firstVAL again, you must reassign firstVAL
+    // val firstVAL = "yes i reassigned"
+    // println(firstVAL)
+}
