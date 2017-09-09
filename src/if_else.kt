9feddcc2ef6d67ab7b108564f4adebe89d8b8a29
selcuk <benimpostahesabim@gmail.com>
2017-09-09 23:56:07 +0300

    if else

---
+++ src/if_else.kt
@@ -0,0 +1,12 @@
+fun main(Args: Array<String>){
+
+    print("enter age: ")
+    var age:Int = readLine()!!.toInt()
+
+    if (age > 18){
+        println("oke come in")
+    } else  {
+        println("only 18 years old")
+    }
+
+}
